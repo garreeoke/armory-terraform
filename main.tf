3aa0f287b94df7a22dab726676713e6e055941a6
garreeoke <garreeoke@gmail.com>
2020-04-17 09:03:03 -0700

    .

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ provider "google" {
 resource "google_compute_instance_template" "blazer-template" {
   name         = "blazer1"
   machine_type = "n1-standard-1"
+  source_instance_template = "blazer-template"
 
   network_interface {
     network = "default"
