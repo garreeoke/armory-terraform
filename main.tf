887d96effa725b635e6326fe184d75c991a2f991
garreeoke <garreeoke@gmail.com>
2020-04-18 09:05:09 -0700

    from image

--- main.tf
+++ main.tf
@@ -1,14 +1,19 @@
 provider "google" {
-  credentials = "${file("/home/spinnaker/.google/gcp.json")}"
+  credentials = file("/Users/aarontorgerson/clouds/gke/spinnaker-gcs.json")
   project = "aaront-armory1"
   region  = "us-west1"
   zone    = "us-west1-b"
 }
 
-resource "google_compute_instance_from_template" "blazer-template" {
-  name         = "instance-from-template"
-  source_instance_template = "blazer-instance-template-1"
+resource "google_compute_instance" "blaze-instance" {
+  name         = "instance-from-image"
+  machine_type = "n1-standard-1"
 
+  boot_disk {
+    initialize_params {
+      image = "blaze-image1"
+    }
+  }
   network_interface {
     network = "default"
   }
